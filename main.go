69c81e2fbe5272a2f1d6ed734a0d09defb3f015c
thatretrodev <thatretrodev@outlook.com>
2022-06-08 19:11:59 -0400

    Add a basic package creation system

--- main.go
+++ main.go
@@ -13,6 +13,35 @@ func main() {
 	fmt.Println("~-~-Slice!~-~-")
 
 	switch os.Args[1] {
+	case "create":
+		// Read metadata file
+
+		data, err := ioutil.ReadFile(os.Args[2] + "/metadata.json")
+
+		if err != nil {
+			panic(err)
+		}
+
+		// Parse metadata file
+
+		var packageMetadata slicepackage.Package
+
+		err = json.Unmarshal(data, &packageMetadata)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Creating package: " + packageMetadata.Name + " (Architecture: " + packageMetadata.Architecture + ")...")
+
+		data, err = slicepackage.CreatePackageTarball(os.Args[2])
+
+		if err != nil {
+			panic(err)
+		}
+
+		ioutil.WriteFile(os.Args[3], slicepackage.CompressData(data), 775)
+
 	case "install":
 		// Read file
 
@@ -32,7 +61,7 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Println("Installing " + packageMetadata.Name + " (Architecture: " + packageMetadata.Architecture + ")...")
+		fmt.Println("Installing package: " + packageMetadata.Name + " (Architecture: " + packageMetadata.Architecture + ")...")
 	default:
 		fmt.Println("???")
 	}
--- slicepackage/slicepackage.go
+++ slicepackage/slicepackage.go
@@ -1,5 +1,16 @@
 package slicepackage
 
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Package struct {
 	Name         string   `json:"name"`
 	Version      string   `json:"version"`
@@ -7,4 +18,78 @@ type Package struct {
 	Maintainer   string   `json:"maintainer"`
 	Dependencies []string `json:"dependencies"`
 	Description  string   `json:"description"`
+}
+
+func CompressData(data []byte) []byte {
+	buffer := new(bytes.Buffer)
+
+	writer, _ := gzip.NewWriterLevel(buffer, gzip.BestCompression)
+
+	writer.Write(data)
+
+	writer.Close()
+
+	return buffer.Bytes()
+}
+
+func DecompressData(data []byte) []byte {
+	reader, _ := gzip.NewReader(bytes.NewReader(data))
+
+	decompressedData, _ := ioutil.ReadAll(reader)
+
+	return decompressedData
+}
+
+func CreatePackageTarball(source string) ([]byte, error) {
+	buffer := new(bytes.Buffer)
+
+	tarball := tar.NewWriter(buffer)
+
+	defer tarball.Close()
+
+	_, err := os.Stat(source)
+
+	if err != nil {
+		return nil, nil
+	}
+
+	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(info, info.Name())
+
+		if err != nil {
+			return err
+		}
+
+		header.Name = "." + strings.TrimPrefix(path, source)
+
+		if err := tarball.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+
+		if err != nil {
+			return err
+		}
+
+		defer file.Close()
+
+		_, err = io.Copy(tarball, file)
+
+		return err
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return buffer.Bytes(), nil
 }
